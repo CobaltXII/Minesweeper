5a37609e896e9db150a3bfb267ce1bc0d1c9e042
CobaltXII <sidatt64@gmail.com>
2019-06-01 13:14:17 -0400

    Added a Minesweeper class

--- Minesweeper.hpp
+++ Minesweeper.hpp
@@ -40,3 +40,59 @@ struct Cell {
 	bool is_mine;
 	bool is_culprit;
 };
+
+// A Minesweeper game.
+class Minesweeper {
+public:
+	// The graphics adapter.
+	Graphics adapter;
+
+	// The game board.
+	Cell* board;
+
+	// The sprites.
+	Sprite border[9];
+	Sprite tile[16];
+	Sprite counter[10];
+	Sprite smiley[5];
+	Sprite frame;
+
+	// The game's settings.
+	int x_cells;
+	int y_cells;
+	int mines;
+
+	// The game's state.
+	enum {
+		GAME_WAITING,
+		GAME_PLAYING,
+		GAME_WINNER,
+		GAME_LOSER
+	} state;
+	Uint32 start_ticks = 0;
+	Uint32 end_ticks = 0;
+	int flags;
+
+	// The game board's render offset.
+	const int xoff = 10;
+	const int yoff = 50;
+
+	// Default constructor.
+	Minesweeper(int x_cells, int y_cells, int mines) {
+		// Create the graphics adapter.
+		adapter = Graphics("Minesweeper", x_cells * 16 + xoff + 10, y_cells * 16 + yoff + 10, 2);
+		// Load the sprites.
+		load_borders();
+		load_tiles();
+		load_counters();
+		load_smileys();
+		load_frame();
+		// Allocate the game board.
+		this->x_cells = x_cells;
+		this->y_cells = y_cells;
+		this->mines = mines;
+		board = new Cell[x_cells * y_cells];
+		// Generate the game board.
+		generate_board();
+	}
+};
